29ac9449948999e961555ca040cac7f8b7f905a1
Matthieu <matth1325@gmail.com>
2025-11-23 11:33:25 +0100

    migration 2

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -17,7 +17,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     #[ORM\Column]
     private ?int $id = null;
 
-    #[ORM\Column(length: 180)]      //TODO : Ajouter les assert nécessaires
+    #[ORM\Column(length: 180)]      //TODO : Ajouter les assert nécessaires ?
     private ?string $email = null;
 
     /**
