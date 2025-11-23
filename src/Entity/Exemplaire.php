13a1e2bd7cd2027d51e5309f3ae1ec1448f9d2e5
Matthieu <matth1325@gmail.com>
2025-11-23 16:04:05 +0100

    création de la classe Exemplaire

---
+++ src/Entity/Exemplaire.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ExemplaireRepository;
+use DateTimeImmutable;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+#[ORM\Entity(repositoryClass: ExemplaireRepository::class)]
+class Exemplaire
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 6, nullable: true)]
+    private ?string $cote = null;
+
+    #[ORM\Column(length: 20)]
+    #[Assert\NotBlank]
+    private ?string $etat = null;
+
+    #[ORM\Column]
+    #[Assert\NotBlank]
+    private ?bool $disponibilite = null;
+
+    #[ORM\Column]
+    #[Assert\NotBlank]
+    private ?\DateTimeImmutable $createdAt = null;
+
+    // Constructeur
+    public function __contruct()
+    {
+        $this->createdAt = new DateTimeImmutable();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getCote(): ?string
+    {
+        return $this->cote;
+    }
+
+    public function setCote(?string $cote): static
+    {
+        $this->cote = $cote;
+
+        return $this;
+    }
+
+    public function getEtat(): ?string
+    {
+        return $this->etat;
+    }
+
+    public function setEtat(string $etat): static
+    {
+        $this->etat = $etat;
+
+        return $this;
+    }
+
+    public function isDisponibilite(): ?bool
+    {
+        return $this->disponibilite;
+    }
+
+    public function setDisponibilite(bool $disponibilite): static
+    {
+        $this->disponibilite = $disponibilite;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeImmutable
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeImmutable $createdAt): static
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+}
