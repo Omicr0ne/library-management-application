2818ce4ab03b993a1c9011f27b4ac22f0a762f51
Matthieu <matth1325@gmail.com>
2025-11-23 16:05:59 +0100

    migration 4

---
+++ migrations/Version20251123150428.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20251123150428 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE exemplaire (id INT AUTO_INCREMENT NOT NULL, cote VARCHAR(6) DEFAULT NULL, etat VARCHAR(20) NOT NULL, disponibilite TINYINT(1) NOT NULL, created_at DATETIME NOT NULL, PRIMARY KEY (id)) DEFAULT CHARACTER SET utf8mb4');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE exemplaire');
+    }
+}
